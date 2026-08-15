ad8c5d305e7f00ff6366586bb91e4aaf092e53a8
Your Name <your_email@example.com>
2026-08-15 11:02:49 +0530

    Update next config domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,11 +5,15 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'images.unsplash.com',
       },
       {
-        protocol: 'http',
-        hostname: '**',
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
       }
     ],
   },
